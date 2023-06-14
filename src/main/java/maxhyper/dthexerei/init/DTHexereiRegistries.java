a67f16fdcedddd1a50d6d1d0d5ba9100dc0fd0c5
Max Hyper <massimocantugarcia@gmail.com>
2023-06-14 15:19:58 -0300

    Initial commit

---
+++ src/main/java/maxhyper/dthexerei/init/DTHexereiRegistries.java
@@ -0,0 +1,18 @@
+package maxhyper.dthexerei.init;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DTHexereiRegistries {
+
+//    @SubscribeEvent
+//    public static void registerPodType(final TypeRegistryEvent<Pod> event) {
+//        event.registerType(DynamicTreesArsNouveau.location("falling_palm"), FallingCoconutPod.TYPE);
+//    }
+//
+//    @SubscribeEvent
+//    public static void onGenFeatureRegistry (final RegistryEvent<GenFeature> event) {
+//        DTEcologicsGenFeatures.register(event.getRegistry());
+//    }
+
+}
